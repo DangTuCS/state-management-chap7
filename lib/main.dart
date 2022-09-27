286195e482c84f958e7bf4c4917c9539dba36580
DangTuCS <dangtu12350@gmail.com>
2022-09-27 23:34:42 +0700

    state-complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
 
 void main() {
   runApp(const MyApp());
@@ -86,6 +87,39 @@ extension AddRemoveItems<T> on Iterable<T> {
 
   Iterable<T> operator -(T other) => where((element) => element != other);
 }
+
+Iterable<String> addItemReducer(
+  Iterable<String> previousItems,
+  AddItemAction action,
+) =>
+    previousItems + action.item;
+
+Iterable<String> removeItemReducer(
+  Iterable<String> previousItems,
+  RemoveItemAction action,
+) =>
+    previousItems - action.item;
+
+Reducer<Iterable<String>> itemsReducer = combineReducers<Iterable<String>>([
+  TypedReducer<Iterable<String>, AddItemAction>(addItemReducer),
+  TypedReducer<Iterable<String>, RemoveItemAction>(removeItemReducer),
+]);
+
+ItemFilter itemFilterReducer(
+  State oldState,
+  Action action,
+) {
+  if (action is ChangeFilterTypeAction) {
+    return action.filter;
+  } else {
+    return oldState.filter;
+  }
+}
+
+State appStateReducer(State oldState, Action action) => State(
+      items: itemsReducer(oldState.items, action),
+      filter: itemFilterReducer(oldState, action),
+    );
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
